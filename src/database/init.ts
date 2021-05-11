3e30fa661b918e38e115106bb3b86b73cf50c5b4
nqdai <nguyendai1992@gmail.com>
2021-05-12 00:06:25 +0700

    Init mock Posgres database from csv

--- src/database/init.ts
+++ src/database/init.ts
@@ -1,16 +1,22 @@
+import fs from 'fs';
+import path from 'path';
 import { Client } from 'pg';
+import { from as copyFrom } from 'pg-copy-streams';
 import TableNames from './enum/TableNames';
 
 export default function initDatabase(client: Client): void {
   client.query(`
-    CREATE TABLE IF NOT EXISTS ${TableNames.ARTICLE} (
-        id int,
+    DROP TABLE IF EXISTS ${TableNames.ARTICLE};
+    CREATE TABLE ${TableNames.ARTICLE} (
+        id int primary key,
         authorId int,
         title text,
         type text,
         description text,
         content text
     );
-    ALTER TABLE ${TableNames.ARTICLE} ADD PRIMARY KEY (id);
   `);
+  const stream = client.query(copyFrom(`COPY ${TableNames.ARTICLE} FROM STDIN DELIMITER ',' CSV`));
+  const fileStream = fs.createReadStream(path.join(__dirname, './mockData/article.csv'));
+  fileStream.pipe(stream);
 }
